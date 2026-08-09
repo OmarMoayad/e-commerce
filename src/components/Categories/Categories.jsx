6adbd878cb012f1c49a08dd0bd9ff2a65686ecf6
Omar Moayad <omarmoayad6@gmail.com>
2026-08-09 05:56:37 +0300

    categories text color  section fix on render

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -35,7 +35,7 @@ export default function Categories() {
             <Box sx={{ width: "100%", display: "grid", gridTemplateColumns: { xs: "100%", md: "50% 50%", }, gap: 2, boxSizing: "border-box", }}>
                 {categories.map((category) => (
                     <Box key={category.id} sx={{height: 100,display: "flex",alignItems: "center",justifyContent: "flex-start",px: 3,boxSizing: "border-box",border: "1px solid",borderColor: "divider",cursor: "pointer",transition: "all 0.3s ease","&:hover": {backgroundColor: "#333232ff",},}}>
-                        <Typography sx={{fontSize: "1.1rem",fontWeight: 700,textTransform: "uppercase",letterSpacing: 1.5,}}>
+                        <Typography sx={{fontSize: "1.1rem",fontWeight: 700,textTransform: "uppercase",letterSpacing: 1.5, color: "text.primary",}}>
                             {category.name}
                         </Typography>
                     </Box>
