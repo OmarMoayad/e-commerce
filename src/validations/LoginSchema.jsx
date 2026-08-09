f78b336f53e80027b6788311f064911e02f267a1
Omar Moayad <omarmoayad6@gmail.com>
2026-08-09 12:06:10 +0300

    updating schema errors

--- src/validations/LoginSchema.jsx
+++ src/validations/LoginSchema.jsx
@@ -1,5 +1,6 @@
 import * as yup from "yup"
 
 export const loginSchema = yup.object({
-    email: yup.string().required().email(),
-    password: yup.string().required().min(8).max(32),})
+    email: yup.string().required("Email is required").email("Email is invalid"),
+    password: yup.string().required("Password is required").min(8, "Password must be at least 8 characters long").max(32, "Password must be at most 32 characters long"),
+})
--- src/validations/RegisterSchema.jsx
+++ src/validations/RegisterSchema.jsx
@@ -1,9 +1,9 @@
 import * as yup from "yup"
 
 export const registerSchema = yup.object({
-    userName: yup.string().required().min(3).max(30),
-    fullName: yup.string().required().min(3).max(30),
-    email: yup.string().required().email(),
-    phoneNumber: yup.string().required().min(9).max(15),
-    password: yup.string().required().min(8).max(32),})
+    userName: yup.string().required("User name is required").min(3, "User name must be at least 3 characters long").max(30, "User name must be at most 30 characters long"),
+    fullName: yup.string().required("Full name is required").min(3, "Full name must be at least 3 characters long").max(30, "Full name must be at most 30 characters long"),
+    email: yup.string().required("Email is required").email("Email is invalid"),
+    phoneNumber: yup.string().required("Phone number is required").min(9, "Phone number must be at least 9 characters long").max(15, "Phone number must be at most 15 characters long"),
+    password: yup.string().required("Password is required").min(8, "Password must be at least 8 characters long").max(32, "Password must be at most 32 characters long")})
     
